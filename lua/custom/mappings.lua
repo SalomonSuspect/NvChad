6e56cfeab6566d6c24166536e8e2f5654fd6c83e
Ryan bonavida <rbonavida@sram.com>
2024-02-23 09:34:03 -0700

    Load harpoon correctly and setup key binding

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,8 +1,19 @@
  local M = {}
-  M.customTelescope = {
+ M.customTelescope = {
    n = {
     ["<leader>gw"] = { "<cmd> Telescope  git_files<CR>", "Find git file" },
-   }
+   },
+ }
+ M.harpoon = {
+   n = {
+      ["<leader>l"] = {
+        function()
+           local harpoon = require("harpoon")
+           --harpoon.setup()
+           harpoon.ui:toggle_quick_menu(harpoon:list())
+        end, "Harpoon list file",
+   },
+ }
  }
 
 
